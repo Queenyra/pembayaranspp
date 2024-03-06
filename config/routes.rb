62a215fe54be32ac3731b543ca65f1777ec3c003
web <yenierara06@gmail.com>
2024-03-06 08:17:56 +0700

    gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :invoices
   resources :categories
   resources :payments
   resources :years
   resources :students
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
